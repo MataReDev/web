38045871237c74c45345e7b7418ca71905033025
MataReDev <goudalmathys@gmail.com>
2023-04-22 12:33:18 +0200

    Commentaires

--- src/components/VideoPage/Commentaires.js
+++ src/components/VideoPage/Commentaires.js
@@ -1,59 +1,59 @@
 import React, { useState, useContext } from "react";
-
+import {fakeCommentaire} from './fakeCommentaires'
 function getAuthToken() {
-  return localStorage.getItem('authToken');
+  return localStorage.getItem("authToken");
 }
 
 function Commentaires() {
   const [commentaire, setCommentaire] = useState("");
+  const [commentaires, setCommentaires] = useState(fakeCommentaire);
 
   const handleCommentaireSubmit = (event) => {
     event.preventDefault();
 
-    if (getAuthToken()) {
+    if (commentaire && getAuthToken()) {
       fetch("/api/addcommentaire", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ commentaire }),
       })
         .then((response) => response.json())
         .then((data) => {
-          // TODO: Traiter la réponse de l'API
+          setCommentaire("");
+          setCommentaires([...commentaires, data]);
         });
     }
   };
 
   return (
-    <div className="border border-black p-5 rounded-xl h-fit w-full">
+    <div className="p-5 rounded-xl h-full">
       <h1 className="font-bold">Commentaires</h1>
       <div className="flex flex-col gap-2">
-          Ajouter un Commentaires :
-        <form className="flex gap-5">
-          <input type="text" name="commentaire" className="bg-gray-200 w-3/4 rounded-xl p-2" placeholder="Ecrivez du texte ici ..." />
-          <input type="submit" value="Envoyer" className="w-1/4 rounded-xl border border-black hover:bg-gray-200" />
+        Ajouter un Commentaire :
+        <form className="flex gap-5" onSubmit={handleCommentaireSubmit}>
+          <input
+            type="text"
+            value={commentaire}
+            onChange={(event) => setCommentaire(event.target.value)}
+            className="bg-gray-200 w-3/4 rounded-xl p-2"
+            placeholder="Ecrivez du texte ici ..."
+          />
+          <input
+            type="submit"
+            value="Envoyer"
+            className="w-1/4 rounded-xl border border-black hover:bg-gray-200"
+          />
         </form>
       </div>
-      <div className="flex flex-col gap-4 p-5">
-        <div className="border border-black p-3 rounded-lg">
-            Auteur - Date
-            <div>
-                Contenu du commentaire
-            </div>
-        </div>
-        <div className="border border-black p-3 rounded-lg">
-            Auteur - Date
-            <div>
-                Contenu du commentaire
-            </div>
-        </div>
-        <div className="border border-black p-3 rounded-lg">
-            Auteur - Date
-            <div>
-                Contenu du commentaire
-            </div>
-        </div>
+      <div className="flex flex-col gap-4 pt-5">
+        {commentaires.map((commentaire) => (
+          <div key={commentaire.id} className="p-2 rounded-xl bg-gray-200">
+            {commentaire.auteur} - {commentaire.date}
+            <div>{commentaire.contenu}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
