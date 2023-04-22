0e3fa977d665a7dc0a63fdcdc1959a9b66cfca39
MataReDev <goudalmathys@gmail.com>
2023-04-22 18:09:29 +0200

    Add debig view

--- src/components/VideoPage/Commentaires.js
+++ src/components/VideoPage/Commentaires.js
@@ -1,21 +1,25 @@
 import React, { useState, useContext } from "react";
-import {fakeCommentaire} from './fakeCommentaires'
+import { fakeCommentaire } from "./fakeCommentaires";
+
 function getAuthToken() {
   return localStorage.getItem("authToken");
 }
 
 function Commentaires() {
   const [commentaire, setCommentaire] = useState("");
-  const [commentaires, setCommentaires] = useState(fakeCommentaire);
+  const [commentaires, setCommentaires] = useState("");
+  //pour tester l'affichage useState(fakeCommentaire)
 
   const handleCommentaireSubmit = (event) => {
+    console.log("submit",commentaire);
     event.preventDefault();
 
     if (commentaire && getAuthToken()) {
-      fetch("/api/addcommentaire", {
+      fetch(`/api/comment`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          Authorization: `Bearer ${getAuthToken()}`,
         },
         body: JSON.stringify({ commentaire }),
       })
@@ -23,6 +27,63 @@ function Commentaires() {
         .then((data) => {
           setCommentaire("");
           setCommentaires([...commentaires, data]);
+        });
+    }
+  };
+
+  const handleLikeCommentaire = (commentId) => {
+    console.log("like");
+    if (getAuthToken()) {
+      fetch(`/api/comment/like/${commentId}`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${getAuthToken()}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const updatedCommentaires = commentaires.map((commentaire) =>
+            commentaire.id === data.id ? data : commentaire
+          );
+          setCommentaires(updatedCommentaires);
+        });
+    }
+  };
+
+  const handleDislikeCommentaire = (commentId) => {
+    console.log("dislike");
+    if (getAuthToken()) {
+      fetch(`/api/comment/dislike/${commentId}`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${getAuthToken()}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const updatedCommentaires = commentaires.map((commentaire) =>
+            commentaire.id === data.id ? data : commentaire
+          );
+          setCommentaires(updatedCommentaires);
+        });
+    }
+  };
+
+  const handleDeleteCommentaire = (commentId) => {
+    console.log("delete");
+    if (getAuthToken()) {
+      fetch(`/api/comment/${commentId}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${getAuthToken()}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const updatedCommentaires = commentaires.filter(
+            (commentaire) => commentaire.id !== commentId
+          );
+          setCommentaires(updatedCommentaires);
         });
     }
   };
@@ -48,10 +109,31 @@ function Commentaires() {
         </form>
       </div>
       <div className="flex flex-col gap-4 pt-5">
-        {commentaires.map((commentaire) => (
+        {commentaires !== "" &&
+          commentaires.map((commentaire) => (
           <div key={commentaire.id} className="p-2 rounded-xl bg-gray-200">
             {commentaire.auteur} - {commentaire.date}
             <div>{commentaire.contenu}</div>
+            <div className="flex gap-2 pt-2">
+              <button
+                onClick={() => handleLikeCommentaire(commentaire.id)}
+                className="bg-green-500 text-white rounded-xl p-2"
+              >
+                J'aime ({commentaire.likes})
+              </button>
+              <button
+                onClick={() => handleDislikeCommentaire(commentaire.id)}
+                className="bg-red-500 text-white rounded-xl p-2"
+              >
+                Je n'aime pas ({commentaire.dislikes})
+              </button>
+              <button
+                onClick={() => handleDeleteCommentaire(commentaire.id)}
+                className="bg-gray-500 text-white rounded-xl p-2"
+              >
+                Supprimer
+              </button>
+            </div>
           </div>
         ))}
       </div>
