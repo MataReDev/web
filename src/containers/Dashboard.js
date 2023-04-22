2f847ff57a3536f2c380d83e3b299dd309bc44dc
MataReDev <goudalmathys@gmail.com>
2023-04-22 13:17:33 +0200

    Update dashboard

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -47,16 +47,72 @@ export const data = {
       ],
       borderColor: "rgba(255, 99, 132, 1)",
       borderWidth: 2,
+      tension: 0.4,
     },
   ],
 };
 
+async function fetchNbUser() {
+  try {
+    const response = await fetch("/api/nbUser");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(
+      "Erreur lors de la récupération du nombre d'utilisateurs :",
+      error
+    );
+    return null;
+  }
+}
+
+async function fetchNbVideo() {
+  try {
+    const response = await fetch("/api/nbVideo");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(
+      "Erreur lors de la récupération du nombre de vidéos :",
+      error
+    );
+    return null;
+  }
+}
+
+async function fetchNbVues7LastDay() {
+  try {
+    const response = await fetch("/api/nbVideo");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(
+      "Erreur lors de la récupération du nombre de vidéos :",
+      error
+    );
+    return null;
+  }
+}
+
 function Dashboard() {
   const [userCount, setUserCount] = useState(500);
-  const [videoCount, setVideoCount] = useState(1234);
+  const [videoCount, setVideoCount] = useState(137);
+  const [view7lastdays, setView7lastdays] = useState(5343);
+
+  useEffect(() => {
+    async function fetchData() {
+      const nbUser = await fetchNbUser();
+      const nbVideo = await fetchNbVideo();
+      const nbVue7DerniersJour = await fetchNbVues7LastDay();
+      if (nbUser != null) setUserCount(nbUser);
+      if (nbVideo != null) setVideoCount(nbVideo);
+      if (nbVue7DerniersJour != null) setView7lastdays(nbVue7DerniersJour);
+    }
+    fetchData();
+  }, []);
 
   return (
-    <div>
+    <div className="bg-gray-100 p-5">
       <div className="flex flex-col md:flex-row md:justify-between gap-4 p-5">
         <div className="bg-white rounded-md shadow-md p-5 w-full md:w-1/3">
           <h2 className="text-lg font-bold mb-3">Comptes utilisateurs</h2>
@@ -69,10 +125,11 @@ function Dashboard() {
         <div className="bg-white rounded-md shadow-md p-5 w-full md:w-1/3">
           <h2 className="text-lg font-bold mb-3">
             Vues sur les vidéos (7 derniers jours)
+            <p className="text-4xl font-bold">{view7lastdays}</p>
           </h2>
         </div>
       </div>
-      <div className="">
+      <div className="p-10 bg-white rounded-md shadow-lg p-5 w-full">
         <Line options={options} data={data} />
       </div>
     </div>
